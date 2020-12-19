18c5de3f3a30f44a1744d1ad36be5f960fa75832
Patrick Zhen <zhen.patrick@gmail.com>
2020-12-19 17:22:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  songs = []
+  for(let i = 0; i > musicians.length; i++){
+    songs.push(`${musicians[1] plays the ${instruments[i]}}`)
+  }
+}
